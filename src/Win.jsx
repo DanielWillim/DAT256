c3d497c3201a292dfe33644d37c6a86f8f4628d2
Fanny <fannyviksten@gmail.com>
2019-04-11 09:26:24 +0200

    A win screen telling the user their answer is correct

---
+++ src/Win.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+export default class WinScreen extends Component {
+	constructor(){
+		super();
+	}
+	render(){
+		return(<p>Grattis, du hade rätt!
+				<br/>Fortsätt till nästa fråga (Knapp)</p>);
+	}
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Win from 'Win';
 
 ReactDOM.render(
-  <div>Hello World</div>,
+	<Win/>,
   document.getElementById('app'),
 );
+
 
 module.hot.accept();
