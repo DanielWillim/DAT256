687986f98a3b9f89a5f21bc8094798e122ee0839
Anna Sundvall <sunanna@student.chalmers.se>
2019-04-15 14:10:42 +0200

    lagt till frågor

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ import Question from 'question';
 import Win from 'Win';
 import Fail from 'fail';
 
+const questions = [
+{question:"Vem var Chalmers först kvinnliga student?", answers:[["Vera Sandberg", true], ["Elisabeth Bennet", false], ["Anna Sundvall", false], ["Elin Silver", false]]},
+{question:"Vad heter statyn på Götaplatsen?", answers:[["Poseidon", true], ["Pseudonymen", false], ["Presten", false], ["Panamaen", false]]},
+{question:"Vad har Valand fått sitt namn ifrån?", answers:[["Völund", true], ["Valund", false], ["Velund", false], ["Vilund", false]]},
+{question:"Vad heter den långa gatan mellan kungsportsplatsen och götaplatsen?", answers:[["Kungsportsavenyn", true], ["Götaplatsavenyn", false], ["Kungsportsgatan", false], ["Götaplatsgatan", false]]},
+]
+
 export default class App extends Component {
   state = {
     responded: false,
