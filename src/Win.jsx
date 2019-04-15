8590f6a78e26923fd8465f7cded5747000fe83e0
Unknown <mwendt@student.chalmers.se>
2019-04-15 14:11:25 +0200

    Test returns to homepage

--- src/Win.jsx
+++ src/Win.jsx
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 
+import App from 'App'
+
+let test = 0;
+
 export default class Winning extends Component {
-  handleClick() {
+
+  state ={test:0}
+
+  handleClick=()=>{
     console.log('The link was clicked.');
+    this.setState({test:1})
   }
 
   render() {
+    if (this.state.test == 0)
     return (
       <p>
         <h1>Grattis du hade rätt!</h1>
-        <button onClick={this.handleClick}>test</button>
+        <button onClick={this.handleClick}>test!!!</button> 
       </p>
     );
+    else
+    return (
+    <App />
+    )
   }
 }
