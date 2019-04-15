3927d914611801619a0f2e967313c1d1ae28513f
Unknown <mwendt@student.chalmers.se>
2019-04-15 16:42:57 +0200

    Returnknappar funkar, Win e nu en funtion och inte en class
    Gjort så Win e en funtion och lagt funtionalliteten i App istället för i Win/Fail då det är bättre att ha det centrerat

--- src/Win.jsx
+++ src/Win.jsx
@@ -2,26 +2,12 @@ import React, { Component } from 'react';
 
 import App from 'App'
 
-export default class Winning extends Component {
-
-  state ={nextQuestion:0}
-
-  handleClick=()=>{
-    console.log('The link was clicked.');
-    this.setState({nextQuestion:1})
-  }
-
-  render() {
-    if (this.state.nextQuestion == 0)
+export default function Winning(props){
     return (
       <p>
         <h1>Grattis du hade rätt!</h1>
-        <button onClick={this.handleClick}>Fler frågor!</button> 
+        <button onClick={props.onNext}>Fler frågor!</button> 
       </p>
-    );
-    else
-    return (
-    <App />
     )
   }
-}
+
