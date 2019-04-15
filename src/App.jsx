c0e7fadc683e3edcb2874eb4a8a392e7ab78eb68
Fanny <fannyviksten@gmail.com>
2019-04-15 14:11:25 +0200

    Created questions
    Created two questions with answers in an array

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,15 @@ import Question from 'question';
 import Win from 'Win';
 import Fail from 'fail';
 
+const questionArray = [
+{question:"Vilken av följande är INTE en stadsdel i Göteborg?",
+answers:["Kålltorp", false], ["Hallonbergen", true], ["Majorna", false], ["Masthugget", false]]},
+{question:"När var Eurovision song contest senast i Göteborg?",
+answers:["2000", false], ["1975", false], ["1985", true], ["2014", false]]},
+
+];
+
+
 export default class App extends Component {
   state = {
     responded: false,
